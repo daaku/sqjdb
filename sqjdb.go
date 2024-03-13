208d21f687bc52e1a309e3ea364fa89f8504d97a
Naitik Shah <n@daaku.org>
2024-03-13 22:41:50 +0400

    initial commit

---
+++ sqjdb.go
@@ -0,0 +1,109 @@
+package sqjdb
+
+import (
+	"encoding/json"
+	"strings"
+
+	"braces.dev/errtrace"
+	"github.com/davecgh/go-spew/spew"
+	"zombiezen.com/go/sqlite"
+)
+
+func Bind(stmt *sqlite.Stmt, i int, v any) error {
+	switch v := v.(type) {
+	default:
+		return errtrace.Errorf("unexpected value %v of type %T", v, v)
+	case int:
+		stmt.BindInt64(i, int64(v))
+	case int16:
+		stmt.BindInt64(i, int64(v))
+	case int32:
+		stmt.BindInt64(i, int64(v))
+	case int64:
+		stmt.BindInt64(i, int64(v))
+	case bool:
+		stmt.BindBool(i, v)
+	case []byte:
+		stmt.BindBytes(i, v)
+	case float32:
+		stmt.BindFloat(i, float64(v))
+	case float64:
+		stmt.BindFloat(i, v)
+	case nil:
+		stmt.BindNull(i)
+	case string:
+		stmt.BindText(i, v)
+	}
+	return nil
+}
+
+type SQL struct {
+	Query string
+	Args  []any
+}
+
+type Table[T any] struct {
+	Name    string
+	qInsert string
+}
+
+func NewTable[T any](name string) Table[T] {
+	return Table[T]{
+		Name:    name,
+		qInsert: "insert into " + name + " (data) values (jsonb(?))",
+	}
+}
+
+func (t *Table[T]) Insert(conn *sqlite.Conn, doc *T) (*T, error) {
+	// check if ID is set
+	// if not, create shallow copy, set it and use that
+	jsonS, err := json.Marshal(doc)
+	if err != nil {
+		return nil, errtrace.Wrap(err)
+	}
+	stmt := conn.Prep(t.qInsert)
+	stmt.BindText(1, string(jsonS))
+	if _, err := stmt.Step(); err != nil {
+		return nil, errtrace.Errorf("inserting document in %q: %w", t.Name, err)
+	}
+	return doc, nil
+}
+
+func (t *Table[T]) One(conn *sqlite.Conn, sqls ...SQL) (*T, error) {
+	var query strings.Builder
+	query.WriteString("select json(data) from ")
+	query.WriteString(t.Name)
+	for _, part := range sqls {
+		query.WriteRune(' ')
+		query.WriteString(part.Query)
+	}
+	query.WriteString(" limit 1")
+	stmt, err := conn.Prepare(query.String())
+	if err != nil {
+		return nil, errtrace.Wrap(err)
+	}
+	i := 1 // Bind Parameter indices start at 1.
+	for _, part := range sqls {
+		for _, arg := range part.Args {
+			if err := Bind(stmt, i, arg); err != nil {
+				return nil, errtrace.Wrap(err)
+			}
+			i++
+		}
+	}
+	rowReturned, err := stmt.Step()
+	spew.Dump(rowReturned, err)
+	if err != nil {
+		return nil, errtrace.Wrap(err)
+	}
+	if !rowReturned {
+		return nil, nil
+	}
+	// TODO: benchmark if this is better than ColumnReader
+	jsonS := stmt.ColumnText(0)
+	v := new(T)
+	if err := json.Unmarshal([]byte(jsonS), v); err != nil {
+		return nil, errtrace.Errorf("invalid json from db: %w\n%s", err, jsonS)
+	}
+	return v, nil
+}
---
+++ sqjdb_test.go
@@ -0,0 +1,46 @@
+package sqjdb_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/daaku/ensure"
+	"github.com/daaku/sqjdb"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/oklog/ulid/v2"
+	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitex"
+)
+
+type Jedi struct {
+	ID   string
+	Name string
+	Age  int
+}
+
+var yoda = Jedi{
+	ID:   ulid.Make().String(),
+	Name: "yoda",
+	Age:  980,
+}
+
+var jedis = sqjdb.NewTable[Jedi]("jedis")
+
+func newConn(t *testing.T) *sqlite.Conn {
+	conn, err := sqlite.OpenConn(fmt.Sprintf("file:%s?mode=memory&cache=shared", t.Name()))
+	ensure.Nil(t, err)
+	err = sqlitex.ExecuteTransient(conn, "create table jedis (data blob)", nil)
+	ensure.Nil(t, err)
+	return conn
+}
+
+func TestCRUD(t *testing.T) {
+	conn := newConn(t)
+	defer conn.Close()
+	_, err := jedis.Insert(conn, &yoda)
+	ensure.Nil(t, err)
+	yodaFetched, err := jedis.One(conn, sqjdb.SQL{Query: "where data->>'ID' = ?", Args: []any{yoda.ID}})
+	ensure.Nil(t, err)
+	spew.Dump(yodaFetched)
+	ensure.DeepEqual(t, yoda.Name, yodaFetched.Name)
+}
